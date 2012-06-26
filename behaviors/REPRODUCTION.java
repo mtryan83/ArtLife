59ed950c6367363caeccbed0d1c0fe322fd8aec8
flugen <flugen@flugen>
2012-06-26 08:24:12 -0400

    Added REPRODUCTION constructor.

--- behaviors/REPRODUCTION.java
+++ behaviors/REPRODUCTION.java
@@ -1,9 +1,16 @@
 package behaviors;
 
+import java.util.ArrayList;
+
 import artlife.Grid;
 import artlife.Organism;
 
 public class REPRODUCTION extends Behavior {
+
+	public REPRODUCTION(int numBehs, int branches, ArrayList<Integer> n) {
+		super(numBehs, branches, n);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public int perform(Grid grid, Organism self) {
