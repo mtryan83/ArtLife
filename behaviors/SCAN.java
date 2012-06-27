aef080109a69fd8eb197743e73ec0da58b6b48ff
flugen <mtrdraco@hotmail.com>
2012-06-27 14:10:23 -0300

    Began writing SCAN behavior.

--- behaviors/SCAN.java
+++ behaviors/SCAN.java
@@ -7,14 +7,16 @@ import artlife.Organism;
 
 public class SCAN extends Behavior {
 
-	public SCAN(int numBehs, int branches) {
-		super(numBehs, branches);
-		// TODO Auto-generated constructor stub
+	private double[] weights;
+
+	public SCAN(int numBehs) {
+		super(numBehs, 5);
+		weights = new double[];
 	}
 
-	public SCAN(int numBehs, int branches, ArrayList<Integer> n) {
-		super(numBehs, branches, n);
-		// TODO Auto-generated constructor stub
+	public SCAN(int numBehs, ArrayList<Integer> n) {
+		super(numBehs, 5, n);
+		weights = new double[];
 	}
 
 	@Override
@@ -25,14 +27,24 @@ public class SCAN extends Behavior {
 
 	@Override
 	public Behavior clone() {
-		// TODO Auto-generated method stub
-		return null;
+		SCAN temp = new SCAN(next.size(),next);
+		temp.weights = Arrays.copyOf(weights);
+		return temp;
 	}
 
 	@Override
 	public Behavior mutate() {
-		// TODO Auto-generated method stub
-		return null;
+		SCAN temp = clone();
+		double[] del = new double[weights.length];
+		double w;
+		for(int i=0; i<weights.length; i++) {
+			w = r.nextDouble()*(r.nextBoolean()?-1:1)/weights.length;
+			for(int j=0; j<weights.length; j++) {
+				del[j] += (i==j?w:-w)/weights.length;
+			}
+		}
+		for(int i=0; i<weights.length; i++) { temp.weights += del[i]; }
+		return Behaviors.mutate(temp);
 	}
 
 }
