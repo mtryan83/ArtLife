b6168fef3e600c003c3275441eafe0298e970818
flugen <mtrdraco@hotmail.com>
2012-06-29 11:48:44 -0300

    Added random mode ability.

--- artlife/travel.java
+++ artlife/travel.java
@@ -1,5 +1,12 @@
 package artlife;
 
+import java.util.Random;
+
 public enum travel{
     WALK,SWIM,FLY,ICE;
+	private static final travel[] VALUES = values();
+	private static final Random r = new Random()
+	public travel random() {
+		return VALUES[r.nextInt(VALUES.length)];
+	}
 }
