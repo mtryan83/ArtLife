4fe6a421a77e1ad2ba72d8ae4e1774bfdc04035f
flugen <mtrdraco@hotmail.com>
2012-07-05 12:30:05 -0300

    Switching terrain rendering to BufferedImages.

--- artlife/terrain.java
+++ artlife/terrain.java
@@ -1,6 +1,6 @@
 package artlife;
 
-import java.awt.Color;
+import java.awt.*;
 
 public enum terrain{
     LAND(0,40,5,10,new Color(222,184,135)),
@@ -9,14 +9,22 @@ public enum terrain{
     ICE(20,20,20,0,Color.white);
     private int wcost,scost,fcost,icost;
     private Color c;
+    private static BufferedImage img;
     terrain(int w, int s, int f, int i,Color col){
         wcost = w;
         scost = s;
         fcost = f;
         icost = i;
         c = col;
+        img = new BufferedImage(50,50, BufferedImage.TYPE_4BYTE_AGBR);
+        Graphics2D g = img.createGraphics();
+        g.setColor(c);
+        g.fillRect(0,0,img.getWidth(),img.getHeight());
     }
     public Color c(){return c;}
+    public BufferedImage draw(int z) {
+        return img.getSubImage(0,0,z,z);
+    }
     public int cost(travel t){
         switch(t){
         case WALK:
